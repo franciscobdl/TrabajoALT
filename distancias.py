9f1efc09c88667db65e6cadd9d91764cde8d145a
Blanca24P <bperalf@upv.es>
2023-11-10 11:32:31 +0100

    damerau_restricted dos not return the correct results -> fails pass matrix to vectors -> maybe I need 3 instead of 2 vector?

--- distancias.py
+++ distancias.py
@@ -66,18 +66,70 @@ def levenshtein_cota_optimista(x, y, threshold):
 
 def damerau_restricted_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein restringida con matriz
-    # COMPLETAR
-    #return D[lenX, lenY]
-    return 0
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=int)
+    for i in range(1, lenX + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenX + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+                D[i - 2][j - 2] + 1 if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2] else float('inf')
+            )
+    return D[lenX, lenY]
 
 def damerau_restricted_edicion(x, y, threshold=None):
     # partiendo de damerau_restricted_matriz añadir recuperar
     # secuencia de operaciones de edición
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    Vcurrent = np.zeros(lenX + 1, dtype=int)
+    Vprev = np.zeros(lenX + 1, dtype=int)
+    Vcurrent[0]=0
+
+    for i in range(1, lenX + 1):
+       Vcurrent[i] = Vcurrent[i - 1] + 1
+
+    for j in range(1, lenY + 1):
+        Vcurrent, Vprev = Vprev, Vcurrent
+        Vcurrent[0] = Vprev[0] + 1
+        for i in range(1, lenX + 1):
+            Vcurrent[i] = min(
+                Vcurrent[i - 1] + 1,
+                Vprev[i] + 1,
+                Vprev[i - 1] + (x[i - 1] != y[j - 1]),
+                Vprev[i - 2] + 1 if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2] else float('inf'),
+            )
+           
+    # COMPLETAR
+    return Vcurrent[lenX]
 
 def damerau_restricted(x, y, threshold=None):
     # versión con reducción coste espacial y parada por threshold
-     return min(0,threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    Vcurrent = np.zeros(lenX + 1, dtype=int)
+    Vprev = np.zeros(lenX + 1, dtype=int)
+    Vcurrent[0]=0
+
+    for i in range(1, lenX + 1):
+       Vcurrent[i] = Vcurrent[i - 1] + 1
+
+    for j in range(1, lenY + 1):
+        Vcurrent, Vprev = Vprev, Vcurrent
+        Vcurrent[0] = Vprev[0] + 1
+        for i in range(1, lenX + 1):
+            Vcurrent[i] = min(
+                Vcurrent[i - 1] + 1,
+                Vprev[i] + 1,
+                Vprev[i - 1] + (x[i - 1] != y[j - 1]),
+                Vprev[i - 2] + 1 if i > 1 and j > 1 and x[i - 2] == y[j - 1] and x[i - 1] == y[j - 2] else float('inf'),
+            )
+           
+    # COMPLETAR
+    return Vcurrent[lenX]
+    #return min(Vcurrent[lenX],threshold+1) # COMPLETAR Y REEMPLAZAR ESTA PARTE
 
 def damerau_intermediate_matriz(x, y, threshold=None):
     # completar versión Damerau-Levenstein intermedia con matriz
